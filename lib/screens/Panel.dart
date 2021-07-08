0a35b0f050476faca93db958d16ea6807489fdf7
Melchor Dominguez <melchor.dominguez@myaureax.com>
2021-07-08 06:06:40 -0400

    - #24: fix analyze

--- lib/screens/Panel.dart
+++ lib/screens/Panel.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import '../widget/button.dart';
 import '../widget/card.dart';
 
 import '../src/authentication.dart';
